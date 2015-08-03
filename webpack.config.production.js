09d6ce9c91b715e9ec046f10d0661cd9c33b86f8
Lee Bannard <l_bannard@yahoo.co.uk>
2015-08-03 21:20:57 +0100

    fix dist

---
+++ webpack.config.production.js
@@ -0,0 +1,47 @@
+var path = require('path');
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  devtool: 'source-map',
+  entry: {
+    app: './src/main.js'
+  },
+  output: {
+    filename: '[name].min.js',
+    path: path.join(__dirname, 'dist'),
+    publicPath: ''
+  },
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+      },
+      '__DEV_TOOLS__': false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    }),
+    //new ExtractTextPlugin('app.css', { allChunks: true }),
+    new HtmlWebpackPlugin({
+      title: 'Redux Boilerplate',
+      filename: 'index.html',
+      template: 'index.template.html',
+      favicon: path.join(__dirname, 'assets/images/favicon.ico')
+    })
+  ],
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!cssnext-loader') },
+      { test: /\.js$/, loaders: ['babel'], exclude: /node_modules/ }
+    ]
+  },
+  cssnext: {
+    browsers: 'last 2 versions'
+  }
+};
