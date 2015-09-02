785ec64adc07c6c27cbb3ddbc19c8e735e06f40d
Lee Bannard <l_bannard@yahoo.co.uk>
2015-09-02 09:02:48 +0100

    remove root component

--- src/main.js
+++ src/main.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import Root from './components/Root';
+import { Provider } from 'react-redux';
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+import { Router, Route } from 'react-router';
 import { createHistory } from 'history';
+import configureStore from './store/configureStore';
+import HelloWorld from './components/HelloWorld';
 
 const history = createHistory();
+const store = configureStore();
 
 React.render(
-  <Root history={history}/>,
+  <div>
+    <Provider store={store}>
+      {() =>
+        <Router history={history}>
+          <Route path="/" component={HelloWorld} />
+        </Router>
+      }
+    </Provider>
+    {__DEV_TOOLS__ ?
+      <DebugPanel top right bottom>
+        <DevTools store={store}
+          monitor={LogMonitor} />
+      </DebugPanel> : null
+    }
+  </div>,
   document.getElementById('app')
 );
