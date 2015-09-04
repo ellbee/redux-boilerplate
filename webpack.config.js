43ddd66ca32da98e5823182af24c46aa4e5aefa6
Lee Bannard <l_bannard@yahoo.co.uk>
2015-09-04 21:25:21 +0100

    switch to babel-plugin-react-transform

--- webpack.config.js
+++ webpack.config.js
@@ -4,19 +4,19 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'eval',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
+    'webpack-hot-middleware/client',
     './src/main'
   ],
   output: {
-    filename: 'app.js',
     path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
     publicPath: '/assets/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
     new ExtractTextPlugin('app.css', { allChunks: true }),
     new webpack.DefinePlugin({
       __DEV_TOOLS__: JSON.stringify(JSON.parse(process.env.DEV_TOOLS || 'false'))
@@ -29,8 +29,14 @@ module.exports = {
   ],
   module: {
     loaders: [
-      { test: /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader!cssnext-loader') },
-      { test: /\.js$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ }
+      { test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!cssnext-loader') 
+      },
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src')
+      }
     ]
   },
   cssnext: {
